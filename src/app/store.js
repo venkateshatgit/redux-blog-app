fee1b90933a92e84d14dc65acd9094199412fb64
Gunake <venkatesh.gunake@capgemini.com>
2023-05-05 15:44:49 +0530

    Added post slice

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import postsSlice from "../features/postsSlice";
+
+
+export default configureStore({
+    reducer: {
+        posts: postsSlice,
+    }
+})
